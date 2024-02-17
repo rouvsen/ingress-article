989cb1843bb8d8a32c038138ba3a861d3f2a5424
rouvsen <rouvsen@gmail.com>
2024-02-17 22:49:35 +0400

    refactor submodules' Main class Names

--- ms_article1/src/main/java/az/ingress/Article1Application.java
+++ ms_article1/src/main/java/az/ingress/Article1Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Article1Application {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
+        SpringApplication.run(Article1Application.class);
     }
 }
